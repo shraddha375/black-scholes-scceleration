a914fe642594a8d8d07a60522a09b04e7067446f
shraddha375 <39151337+shraddha375@users.noreply.github.com>
2025-12-10 00:04:44 +0530

    Update exp_rom.sv

--- fpga_compute/exp_rom.sv
+++ fpga_compute/exp_rom.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 04:49:45 PM
-// Design Name: 
-// Module Name: exp_rom
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module exp_rom(
     input logic [9:0] addr,
